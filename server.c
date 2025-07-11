4a46e03b79e1dce1e465a321a7533c600fa58984
Riccardo Fani <riccardofani@gmail.com>
2025-07-11 17:27:16 +0200

    server compliant with subject

--- server.c
+++ server.c
@@ -6,32 +6,51 @@
 /*   By: rfani <rfani@student.42firenze.it>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:42:24 by rfani             #+#    #+#             */
-/*   Updated: 2025/04/02 13:50:24 by rfani            ###   ########.fr       */
+/*   Updated: 2025/07/11 17:15:48 by rfani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <signal.h>
 #include "libft/libft.h"
+
+void	sig_handler(int sig, siginfo_t *info, void *context);
 
 int	main(void)
 {
-	int		pid;
-	char	*pid_str;
+	struct sigaction	sa;
+	int	pid;
 
-	pid = getpid();
-	pid_str = ft_itoa(pid);
-	if (pid_str == NULL)
-	{
-		ft_putstr_fd("Error: Failed to convert PID to string\n", 2);
-		return (1);
-	}
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putstr_fd(pid_str, 1);
-	ft_putstr_fd("\n", 1);
-	free(pid_str);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server PID: %d \n", getpid());
 	while (1)
-	{
 		pause();
-	}
 	return (0);
+}
+
+void sig_handler(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_count = 0;
+	static char	c = 0;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		c |= (1 << (7 - bit_count));
+	else if (sig == SIGUSR2)
+		c &= ~(1 << (7 - bit_count));
+	bit_count++;
+	if (bit_count == 8)
+	{
+		if (c == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", c);
+		bit_count = 0;
+		c = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
 }
